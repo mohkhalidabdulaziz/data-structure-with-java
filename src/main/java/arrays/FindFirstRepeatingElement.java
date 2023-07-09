2989a9ece667db03f31d094539f59ef2a6279d10
mohkhalidabdulaziz <mohkhalidabdulaziz@gmail.com>
2023-07-09 21:46:06 +0500

    by sorting technique

--- src/main/java/arrays/FindFirstRepeatingElement.java
+++ src/main/java/arrays/FindFirstRepeatingElement.java
@@ -1,7 +1,10 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class FindFirstRepeatingElement {
 
+    //Bruteforce approach
     public static int getFirstRepeatingElement(int[] arr, int n) {
         // step 1: compare each element to the rest of the array
         // step 2: If found, return index of that element
@@ -15,10 +18,34 @@ public class FindFirstRepeatingElement {
         return -1;
     }
 
+    //Sorting approach using Binary Search
+
+   /*Copy the given array to an auxiliary array temp[] and sort temp array.
+   Traverse the input array arr[] from 0 to N – 1.
+    For every element, count its occurrences in temp[] using binary search.
+    If the count of occurrence of current element is more than one, then return the current element.
+    If no repeating element is found print “No Repeating Number Found”.
+   */
+
+    public static int getFirstRepeatedElementByBinarySearch(int[] arr, int n) {
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == arr[i+1]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+
+
+
     public static void main(String[] args) {
-        int[] arr = {10, 5, 3, 4, 3, 5, 6};
+        int[] arr = {10, 5, 4, 3, 5, 6};
         int n = arr.length;
-        System.out.println(getFirstRepeatingElement(arr,n));
+        //System.out.println(getFirstRepeatingElement(arr,n));
+        System.out.println(getFirstRepeatedElementByBinarySearch(arr,n));
     }
 
 
